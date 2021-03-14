854d8e35a2737612afb2e6a5ba3234faa00a2d17
Léo Garret <leogarret@hotmail.com>
2021-03-14 09:40:26 +0100

    Adding PropertySearch Entity

--- src/Entity/PropertySearch.php
+++ src/Entity/PropertySearch.php
@@ -4,13 +4,44 @@ namespace App\Entity;
 class PropertySearch {
 
     /**
-     * @var Int
+     * @var int|null
      */
     private $maxPrice;
 
     /**
-     * @var Int
+     * @var int|null
      */
     private $minSurface;
+
+    /**
+     * @return int|null
+     */
+    public function getMaxPrice(): ?int {
+        return $this->maxPrice;
+    }
+
+    /**
+     * @param int $maxPrice
+     * @return PropertySearch
+     */
+    public function setMaxPrice($maxPrice): PropertySearch {
+        $this->maxPrice = $maxPrice;
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getMinSurface(): ?int {
+        return $this->minSurface;
+    } 
     
+    /**
+     * @param int $minSurface
+     * @return PropertySearch
+     */
+    public function setMinSurface($minSurface): PropertySearch {
+        $this->minSurface = $minSurface;
+        return $this;
+    }
 }
